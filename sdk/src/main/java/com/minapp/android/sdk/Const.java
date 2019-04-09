4eb65b87be28f26bfa3e9b7fa965cd0c1763b3e9
Lin Wei <linwei@ifanr.com>
2019-04-09 19:02:36 +0800

    http request header 添加 sdk platform 和 sdk version

--- sdk/src/main/java/com/minapp/android/sdk/Const.java
+++ sdk/src/main/java/com/minapp/android/sdk/Const.java
@@ -5,18 +5,16 @@ public abstract class Const {
     public static final String TAG = "minapp-android-sdk";
     public static final String HTTP_HOST = "https://cloud.minapp.com/";
     public static final long HTTP_TIMEOUT = 10 * 1000;                              // http 读、写、连接的超时设置，单位毫秒
-    public static final String AUTH_HEADER_AUTH_PREFIX = "Hydrogen-r1 ";
-    public static final String AUTH_HEADER_AUTH = "authorization";
+    public static final String HTTP_HEADER_AUTH_PREFIX = "Hydrogen-r1 ";
+    public static final String HTTP_HEADER_AUTH = "authorization";
+    public static final String HTTP_HEADER_PLATFORM = "X-Hydrogen-Client-Platform";
+    public static final String HTTP_HEADER_VERSION = "X-Hydrogen-Client-Version";
 
     public static final String HEADER_CONTENT_TYPE = "Content-Type";
     public static final String MEDIA_TYPE_JSON = "application/json";
     public static final String MEDIA_TYPE_FORM = "multipart/form-data";
 
-    public static final String HTTP_POST = "POST";
-    public static final String HTTP_PUT = "PUT";
-    public static final String HTTP_GET = "GET";
-    public static final String HTTP_DELETE = "DELETE";
-
-    public static final String AUTH_HEADER_CLIENT_ID = "X-Hydrogen-Client-ID";
+    public static final String HTTP_HEADER_CLIENT_ID = "X-Hydrogen-Client-ID";
+    public static final String SDK_PLATFORM = "NATIVE_ANDROID";
 
 }
--- sdk/src/main/java/com/minapp/android/sdk/auth/AuthInterceptor.java
+++ sdk/src/main/java/com/minapp/android/sdk/auth/AuthInterceptor.java
@@ -1,5 +1,6 @@
 package com.minapp.android.sdk.auth;
 
+import com.minapp.android.sdk.BuildConfig;
 import com.minapp.android.sdk.Const;
 import com.minapp.android.sdk.exception.UninitializedException;
 import okhttp3.Interceptor;
@@ -14,15 +15,20 @@ public class AuthInterceptor implements Interceptor {
     @Override
     public okhttp3.Response intercept(Chain chain) throws IOException {
         Request.Builder builder = chain.request().newBuilder();
+
         String clientId = Auth.clientId();
         if (clientId == null) {
             throw new IOException(new UninitializedException());
         }
-        builder.header(Const.AUTH_HEADER_CLIENT_ID, clientId);
+        builder.header(Const.HTTP_HEADER_CLIENT_ID, clientId);
+
         String token = Auth.token();
         if (token != null) {
-            builder.header(Const.AUTH_HEADER_AUTH, Const.AUTH_HEADER_AUTH_PREFIX + token);
+            builder.header(Const.HTTP_HEADER_AUTH, Const.HTTP_HEADER_AUTH_PREFIX + token);
         }
+
+        builder.header(Const.HTTP_HEADER_PLATFORM, Const.SDK_PLATFORM);
+        builder.header(Const.HTTP_HEADER_VERSION, BuildConfig.VERSION_NAME);
         return chain.proceed(builder.build());
     }
 }
