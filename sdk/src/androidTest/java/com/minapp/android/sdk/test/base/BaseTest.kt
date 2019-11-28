6a305ab0a2726756ab1bf64780e6a77794b0303a
Lin Wei <linwei@ifanr.com>
2019-11-28 14:11:09 +0800

    BAAS-5776 #comment 添加获取服务器时间接口: BaaS.getServerDate()

--- sdk/src/androidTest/java/com/minapp/android/sdk/test/base/BaseTest.kt
+++ sdk/src/androidTest/java/com/minapp/android/sdk/test/base/BaseTest.kt
@@ -4,22 +4,21 @@ import android.app.Application
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.minapp.android.sdk.BaaS
 import com.minapp.android.sdk.test.TestConst
 import org.junit.After
-import org.junit.BeforeClass
 import org.junit.runner.RunWith
 
-abstract class BaseAndroidTest: BaseTest() {
-    companion object {
+@RunWith(AndroidJUnit4::class)
+abstract class BaseTest {
 
-        @BeforeClass
-        @JvmStatic
-        fun registerSdk() {
-            BaaS.init(
-                TestConst.BAAS_CLIENT_ID,
-                app
-            )
-        }
+    companion object {
+        val ctx: Context = ApplicationProvider.getApplicationContext()
+        val app: Application = ApplicationProvider.getApplicationContext()
     }
+
+    @After
+    fun delay() {
+        Thread.sleep(TestConst.NETWORK_DELAY)
+    }
+
 }
